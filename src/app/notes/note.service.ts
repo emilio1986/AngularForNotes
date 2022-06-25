2bf8acbbffc1b4f685fc9cb56f2a40185b68f595
Emilio Ferraro <emilio_1986win@hotmail.com>
2022-06-25 20:02:59 -0300

    HttpClientWorking

--- src/app/notes/note.service.ts
+++ src/app/notes/note.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Note } from './note';
-import { NOTES } from './notes.json';
 import { Observable, of } from 'rxjs';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class NoteService {
-  constructor() {}
+
+private urlEndpoint:string = 'http://localhost:8080/api/notes';
+
+  constructor(private http:HttpClient) {}
 
   //Any changes ig Observed by this method and autoActuallized
+
   getNotes(): Observable<Note[]> {
-    return of(NOTES);
+
+      //Casting to []Note otherWise always is expecting an Observale/
+    return  this.http.get<Note[]>(this.urlEndpoint);
   }
 }
+
